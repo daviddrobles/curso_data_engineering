2275a63bb2602e9193fa83fddd117e90d6d85b9b
David Robles <davidroblesrodriguez8@gmail.com>
2024-05-24 17:55:27 +0000

    Guardado 4 - 24/05/2024 - 19:55

--- analyses/gen_code.sql
+++ analyses/gen_code.sql
@@ -1,3 +1,4 @@
 {{ codegen.generate_model_yaml(
-    model_names=['stg_google_sheets__budget']
+    model_names=['stg_SQL_SERVER_DBO__addresses', 'stg_SQL_SERVER_DBO__events', 'stg_SQL_SERVER_DBO__order_items', 'stg_SQL_SERVER_DBO__orders', 'stg_SQL_SERVER_DBO__products'
+    , 'stg_SQL_SERVER_DBO__promos', 'stg_SQL_SERVER_DBO__users']
 ) }}
